a2aaf7d778d0f6030410414d9433c184e3ea177d
Kavita Pat Pingua <kavitapatp@gmail.com>
2026-03-31 15:23:11 +0530

    Initial comit - ToDo App

---
+++ script.js
@@ -0,0 +1,50 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks(){
+    let list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        let li = document.createElement("li");
+        li.textContent = task.text;
+
+        if(task.completed){
+            li.classList.add("Completed");
+        }
+
+        li.onclick = () => {
+            tasks[index].completed = !tasks[index].completed;
+            saveTasks();
+        };
+
+        let delBtn = document.createElement("button");
+        delBtn.textContent = "X";
+        delBtn.onclick = (e) => {
+            e.stopPropagation();
+            tasks.splice(index, 1);
+            saveTasks();
+        };
+
+        li.appendChild(delBtn);
+        list.appendChild(li);
+    });
+}
+
+function addTask() {
+    let input = document.getElementById("taskInput");
+
+    if (input.value === "") return;
+
+    tasks.push({ text: input.value, completed: false });
+    input.value = "";
+    saveTasks();
+}
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+}
+
+renderTasks();
+
+   
